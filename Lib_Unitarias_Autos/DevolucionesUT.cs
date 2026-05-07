929b0ff3daf6f1720d50305ab6dd8059fc86abf1
Isabv13 <isabelabohada1129765@correo.itm.edu.co>
2026-05-07 11:43:40 -0500

    Se creo la libreria y las 23 clases de las pruebas unitarias

---
+++ Lib_Unitarias_Autos/DevolucionesUT.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib_Unitarias_Autos
+{
+    internal class DevolucionesUT
+    {
+    }
+}
---
+++ Lib_Unitarias_Autos/PersonasUT.cs
@@ -0,0 +1,16 @@
+﻿using Lib_Negocio_Autos.Implementaciones;
+using Lib_Negocio_Autos.Interfaces;
+
+namespace Lib_Unitarias_Autos 
+{
+    [TestClass]
+    public sealed class PersonasUT
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           
+        }
+    }
+}
+
